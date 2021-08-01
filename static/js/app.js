2b7b3354f7f883d6cef1d22824bc679bca256c6e
unknown <dany.roman.phd@gmail.com>
2021-08-01 09:37:03 -0500

    uploaded final files

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,90 @@
-console.log(data[0])
+console.log("import:", data[0])
 
+// create variables for each json key
 let metadata = data[0].metadata
 let names = data[0].names
 let samples = data[0].samples
 
-let sortbyotu = samples.sort((a, b) => b.sample_values - a.sample_values);
+// create function to initiate the entry display
+function init() {
+    console.log(filterid("940"))
 
-// let person_id = sortbyotu.map(sample => sample.id)
-// let otu_ids = sortbyotu.map(sample => sample.otu_ids);
-// let sample_values = sortbyotu.map(sample => sample.sample_values);
+    // set dropdown menu to display patient ids
+    names.forEach(id => {
+        let dropdownmenu = d3.select("#selDataset")
+        dropdownmenu.append("option").text(id).property("value", id);
+    });
 
-function filterid(indiv) {
-    let person = samples.filter(person => person.id == indiv);
-    let person_data = person[0]
-    let otu_id = person_data.otu_ids
-
-    let top_10_otu_id = otu_id.slice(0, 10);
-    let top_10_otuid_label = top_10_otu_id.map(item => `OTU-${item}`)
-
-    let otu_val = person_data.sample_values
-    let top_10_otu_val = otu_val.slice(0, 10);
-
-    let dataset = {
-        "otulabel": top_10_otuid_label,
-        "otuid": top_10_otu_id,
-        "values": top_10_otu_val
-    }
-
-    plotbar(dataset);
-    plotbubble(dataset);
+    // set patient 940 as first displayed
+    let first_id = "940"
+    filterid(first_id)
 }
 
+/// change the display when a patient is selected
+d3.selectAll("#selDataset").on("change", updateplot);
+
+/// create a function that updates the graphs
+function updateplot() {
+    let update_id = d3.select("#selDataset").node().value;
+
+    d3.selectAll("#table").remove();
+
+    filterid(update_id);
+}
+
+/// create a function that calls for the patient data and displays it
+function filterid(indiv) {
+    /// filter for the demographic data of the patient of interest
+    let identifiers = metadata.filter(person => person.id == indiv);
+
+    /// create variables that hold demographic data
+    let ethnicity = identifiers[0].ethnicity;
+    let gender = identifiers[0].gender;
+    let age = identifiers[0].age;
+    let location = identifiers[0].location;
+    let id = identifiers[0].id;
+    let num_wash = identifiers[0].wfreq;
+
+    /// filter patient sample data for the patient of interest
+    let person = samples.filter(person => person.id == indiv);
+
+    /// create variables to hold the sample data
+    let person_data = person[0]
+    let otu_id = person_data.otu_ids
+    let otu_val = person_data.sample_values
+    let otu_labels = person_data.otu_labels
+    let otuid_label = otu_id.map(item => `OTU-${item}`);
+
+    /// create a dictionary to recall demographics data
+    let demoset = {
+        "patientid": id,
+        "ethnicity": ethnicity,
+        "gender": gender,
+        "age": age,
+        "location": location,
+        "num_wash": num_wash
+    }
+
+    /// create a dictionary to recall sample data
+    let dataset = {
+        "otulabel": otu_labels,
+        "otuid": otu_id,
+        "otuid_label": otuid_label,
+        "values": otu_val
+    };
+
+    /// plot the graphs based on the sample and demographics data
+    plotbar(dataset);
+    plotbubble(dataset);
+    plotdemographics(demoset);
+    plotgauge(demoset);
+}
+
+/// create a bar plot that displays top 10 sample OTUs
 function plotbar(dataset) {
     let barchart = {
-        y: dataset.otulabel.reverse(),
-        x: dataset.values.reverse(),
+        y: dataset.otuid_label.slice(0, 10).reverse(),
+        x: dataset.values.slice(0, 10).reverse(),
         orientation: 'h',
         type: "bar"
     };
@@ -44,6 +94,7 @@ function plotbar(dataset) {
     Plotly.newPlot("bar", datasets);
 }
 
+/// create a bubble chart that displays all sample data for the patient
 function plotbubble(dataset) {
     let bubblechart = {
         x: dataset.otuid,
@@ -62,4 +113,45 @@ function plotbubble(dataset) {
     Plotly.newPlot("bubble", datasets);
 }
 
-console.log(filterid("940"))
+/// create a list of patient demographics data to display
+function plotdemographics(demoset) {
+    let table = d3.select('#sample-metadata');
+    table.selectAll('li').remove();
+
+    table.append("li").text(`Patient ID: ${demoset.patientid}`);
+    table.append("li").text(`Ethnic: ${demoset.ethnicity}`);
+    table.append("li").text(`Gender: ${demoset.gender}`);
+    table.append("li").text(`Age: ${demoset.age}`);
+    table.append("li").text(`Area: ${demoset.location}`);
+}
+
+
+/// create a handwashing gauge
+function plotgauge(demoset) {
+    let data = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: demoset.num_wash,
+            title: { text: "Handwashing Times" },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+                axis: { range: [null, 10] },
+                bar: { color: "#FFFFFF" },
+                steps: [
+                    { range: [0, 2], color: "#AAC9CE" },
+                    { range: [2, 4], color: "#B6B4C2" },
+                    { range: [4, 6], color: "#C9BBC8" },
+                    { range: [6, 8], color: "#E5C1CD" },
+                    { range: [8, 10], color: "#F3DBCF" },
+                ]
+            }
+        }
+    ];
+
+    let layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', data, layout);
+}
+
+/// run first display upon opening the webpage
+init();
--- static/js/bonus.js
+++
@@ -1,6 +0,0 @@
-const file = '/data/samples.json'
-
-const dataPromise = d3.json(file)
-console.log("Data Promise: ", dataPromise)
-
-d3.json(file).then(data => console.log(data));
